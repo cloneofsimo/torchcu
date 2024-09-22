4597eb6ed4ebd2a56685a9558bc133759f7965fe
hanchchch <cndghks15@gmail.com>
2024-09-21 18:56:06 -0700

    fix: delete unexecutable pytorch codes

--- generator/pipeline.py
+++ generator/pipeline.py
@@ -34,11 +34,6 @@ class GeneratorPipeline:
     def generate_input_signature(self, filepath: str):
         generate_signature(filepath)
 
-    def delete_invalid_files(self, filepath: str):
-        if not is_valid_python([filepath]):
-            logger.warning(f"Invalid python code: {filepath}")
-            os.remove(filepath)
-
     def run_pytorch_file(self, filepath: str) -> torch.Tensor | tuple[torch.Tensor]:
         function_name, signature = load_function_signature(filepath)
         pytorch_func = load_pytorch_function(filepath, function_name)
@@ -46,17 +41,35 @@ class GeneratorPipeline:
         inputs = prepare_inputs(signature)
         output = run_pytorch_function(pytorch_func, inputs)
 
-        logger.info(f"Output: {output.shape}, {output.dtype}")
         return output
+
+    def validate_output(self, output: torch.Tensor | tuple[torch.Tensor]):
+        def validate_tensor(tensor: torch.Tensor):
+            if len(tensor.shape) == 0:
+                raise ValueError("We don't need scalar outputs for now.")
+
+        if isinstance(output, tuple):
+            for tensor in output:
+                if not isinstance(tensor, torch.Tensor):
+                    raise ValueError(f"Invalid output type: {type(tensor)}")
+                validate_tensor(tensor)
+        elif isinstance(output, torch.Tensor):
+            validate_tensor(output)
+        else:
+            raise ValueError(f"Invalid output type: {type(output)}")
 
     def get_output_signature(self, output: torch.Tensor | tuple[torch.Tensor]):
         def get_signature(tensor: torch.Tensor):
-            shape = f'({", ".join(tensor.shape)})'
+            shape = f'({", ".join([str(s) for s in tensor.shape])})'
             dtype = tensor.dtype
             return f"({shape}, {dtype})"
 
         if isinstance(output, tuple):
-            return ",\n".join([get_signature(tensor) for tensor in output])
+            return (
+                "\n        "
+                + ",\n        ".join([get_signature(tensor) for tensor in output])
+                + "\n    "
+            )
         elif isinstance(output, torch.Tensor):
             return get_signature(output)
         else:
@@ -85,6 +98,29 @@ class GeneratorPipeline:
         py_filepaths = self.generate_functions(reference_model)
         for filepath in py_filepaths:
             self.generate_input_signature(filepath)
-            self.delete_invalid_files(filepath)
-            output = self.run_pytorch_file(filepath)
+
+            if not is_valid_python([filepath]):
+                logger.warning(f"Invalid python code: {filepath}")
+                os.remove(filepath)
+                continue
+
+            try:
+                output = self.run_pytorch_file(filepath)
+            except Exception as e:
+                logger.warning(
+                    f"Failed to run PyTorch function: {filepath}\nRemoving file."
+                )
+                logger.exception(e)
+                os.remove(filepath)
+                continue
+
+            try:
+                self.validate_output(output)
+            except Exception as e:
+                logger.warning(
+                    f"Invalid output from PyTorch function: {filepath}\nError: {e}\nRemoving file.\n"
+                )
+                os.remove(filepath)
+                continue
+
             self.write_output_signature(filepath, output)
